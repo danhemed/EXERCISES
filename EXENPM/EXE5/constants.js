5675a3c60aa7d95953258c796a15b30ac04d5d8d
Dan <danhemed123@gmail.com>
2025-06-29 11:30:59 +0300

    finish EXE5 finish all the EXERICES!

---
+++ EXENPM/EXE5/constants.js
@@ -0,0 +1,5 @@
+const host = "localhost";
+
+const port = 3000;
+
+export default {host, port};
---
+++ EXENPM/EXE5/server.js
@@ -0,0 +1,7 @@
+import constants from './constants.js';
+
+const localHost = constants.host;
+
+const serverPort = constants.port;
+
+console.log(`Server running on http://${localHost}:${serverPort}`);
