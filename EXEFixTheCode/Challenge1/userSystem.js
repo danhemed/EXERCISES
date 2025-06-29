8424260ccf9c2106f459f2eddaa0b657ac239d25
Dan <danhemed123@gmail.com>
2025-06-29 17:01:08 +0300

    challenge1

---
+++ EXEFixTheCode/Challenge1/userSystem.js
@@ -0,0 +1,9 @@
+var users = [];
+export function greetUser(name) {
+    if (users.includes(name)) {
+        console.log(`Welcome back, ${name}!`);
+    } else {
+        console.log(`Welcome, ${name}!`);
+        users.push(name);
+    }
+}
