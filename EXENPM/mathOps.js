a9758810d5eab2f3a19f7fe3a9b6b6b5a7d86e20
Dan <danhemed123@gmail.com>
2025-06-29 09:40:05 +0300

    start git

---
+++ EXENPM/mathOps.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (a === 0 || b === 0) {
+        console.log("you can'y dividing in zero!");
+    } else {
+        return a / b;
+    }
+}
+
+export {multiply, divide};
